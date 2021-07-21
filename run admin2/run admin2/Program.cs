d25454bcb740a3d955aa7f62e4e4849d490dd124
theomiddleton <59509832+theomiddleton@users.noreply.github.com>
2021-07-21 19:00:21 +0100

    coed

---
+++ run admin2/run admin2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace run_admin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.ProcessStartInfo info = new ProcessStartInfo("cmd.exe");
+            info.UseShellExecute = true;
+            info.Verb = "runas";
+            Process.Start(info);
+        }
+    }
+}
